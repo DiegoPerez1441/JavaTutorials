452009b60bacf522ed2db38c9864c220d2afc3a9
DiegoPerez1441 <devdiegoperez@gmail.com>
2020-05-28 16:40:11 -0500

    Fixed comments

--- Basics/HelloWorld/src/Hello.java
+++ Basics/HelloWorld/src/Hello.java
@@ -8,7 +8,7 @@
 // The void keyword is another Java keyword which means that the method won't return anything
 
 
-/** Shortcuts
+/* Shortcuts
     // This is a single line comment an can be written with ctrl + / or cmd + /
         the other comment with the * in between the // is a multi-line comment
         **Note** if your comments aren't indented automatically go to:
@@ -17,11 +17,11 @@
     To autocomplete your code press press ctrl + space or cmd + space and press enter
     To autocomplete the main method type out main and press ctrl + space or cmd + space and press enter
     To save your code press ctrl + s or cmd + s
- **/
+ */
 
 public class Hello {
 
-    /**  This is the entry point in your program/code  **/
+    /*  This is the entry point in your program/code  */
     public static void main(String[] args) {
         // This will print out Hello World! to the console once you run it
         // strings have to have "" around them and expressions must end with a ;
