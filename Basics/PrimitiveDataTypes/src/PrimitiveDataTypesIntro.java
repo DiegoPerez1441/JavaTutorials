9f9326b48f332601385587e16984a3d87dbfe030
DiegoPerez1441 <devdiegoperez@gmail.com>
2020-05-28 16:38:40 -0500

    Fixed comments

--- Basics/PrimitiveDataTypes/src/PrimitiveDataTypesIntro.java
+++ Basics/PrimitiveDataTypes/src/PrimitiveDataTypesIntro.java
@@ -1,15 +1,15 @@
-/**  Data cited from https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html  **/
+/*  Data cited from https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html  */
 
 public class PrimitiveDataTypesIntro {
 
-    /**  This is the entry point in your program/code  **/
+    /*  This is the entry point in your program/code  */
     public static void main(String[] args) {
 
-        /**  Different Primitive Data Types  **/
-        /**  Primitive Data Types are different ways of storing data into variables depending on the program's needs  **/
-        /**  [Note]: Signed variables are both positive and negative  **/
+        /*  Different Primitive Data Types  */
+        /*  Primitive Data Types are different ways of storing data into variables depending on the program's needs  */
+        /*  [Note]: Signed variables are both positive and negative  */
 
-        /**
+        /*
             Variables are just a name to represent data stored in memory
 
             You can think of it as a name that you give to an object or something in general, in essence it represents it
@@ -17,16 +17,16 @@ public class PrimitiveDataTypesIntro {
 
             However data comes in all shapes and forms and that is why data types are made to distinguish between them
                 just as a name is different from age in the sense that a name is text while age is composed of a number
-         **/
+         */
 
 
         byte b = 14; // The byte data type is an 8-bit signed two's complement integer. It has a minimum value of -128 and a maximum value of 127 (inclusive). The byte data type can be useful for saving memory in large arrays, where the memory savings actually matters.
         short s = 12; // The short data type is a 16-bit signed two's complement integer. It has a minimum value of -32,768 and a maximum value of 32,767 (inclusive)
         int i = 10000; // The int data type is a 32-bit signed two's complement integer, which has a minimum value of -231 and a maximum value of 231-1
-        /**  [Note]: for long data types you must include a L after the long to define it as a long  **/
+        /*  [Note]: for long data types you must include a L after the long to define it as a long  */
         long l = 1000000L; // The long data type is a 64-bit two's complement integer. The signed long has a minimum value of -263 and a maximum value of 263-1
 
-        /**  [Note]: for float data types you must include a f after the float to convert it from a double to a float  **/
+        /*  [Note]: for float data types you must include a f after the float to convert it from a double to a float  */
         float f = 12.4f; // The float data type is a single-precision 32-bit IEEE 754 floating point.
         double d = 12.46; // The double data type is a double-precision 64-bit IEEE 754 floating point.
         double scientificNotation = 1246e-2; // You can also use Scientific Notation for doubles
@@ -36,19 +36,19 @@ public class PrimitiveDataTypesIntro {
         boolean isItFalse = false;
 
         char character = 'a'; // The char data type is a single 16-bit Unicode character, meaning that is stores a single character
-        /* A String is a special data type that is an object (As shown by the uppercase name), it is a collection of char data types stored
+        /* A String is a special data type (Non-Primitive) that is an object (As shown by the uppercase name), it is a collection of char data types stored
             in an array. So don't worry about it at this moment as it will be covered later */
         String name = "Diego";
 
-        /********************************************************/
-        /**                Overflow experiments                **/
-        /********************************************************/
+        /* ****************************************************** */
+        /*                Overflow experiments                    */
+        /* ****************************************************** */
 
-        /**  Things to consider  **/
-        /**
+        /*  Things to consider   */
+        /*
             A variable can only hold a certain amount of information before it overflows and this is a common cause for errors
             Also adding incompatible data types causes errors
-         **/
+         */
 
         // This just gets the min and max values that an integer can store, so don't worry about the syntax
         int minIntValue = Integer.MIN_VALUE;
@@ -70,7 +70,7 @@ public class PrimitiveDataTypesIntro {
         int maxIntTestLimit = 2147483647;
 
 
-        /**  Same test but with a byte data type  **/
+        /*  Same test but with a byte data type   */
         int minByteValue = Byte.MIN_VALUE;
         int maxByteValue = Byte.MAX_VALUE;
 
