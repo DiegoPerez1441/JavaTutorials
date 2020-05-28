5ea5efe47b5dc3f4f179568e1f2b57e43241cac4
DiegoPerez1441 <devdiegoperez@gmail.com>
2020-05-28 15:14:38 -0500

    Added overflow experiment

---
+++ Basics/PrimitiveDataTypes/src/PrimitiveDataTypesOperations.java
@@ -0,0 +1,12 @@
+public class PrimitiveDataTypesOperations {
+
+    public static void main(String[] args) {
+
+        /**
+            As you learned previously data types are used to distinguish different types of data
+            Here we're going to experiment what kinds of data types can be "combined"
+         **/
+
+    }
+
+}
