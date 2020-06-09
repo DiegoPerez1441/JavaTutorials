82863b242bf374c900310126e329f9acb52a7024
DiegoPerez1441 <devdiegoperez@gmail.com>
2020-06-08 19:09:13 -0500

    Added more detail

--- Basics/PrimitiveDataTypes/src/ParsingValuesFromAString.java
+++ Basics/PrimitiveDataTypes/src/ParsingValuesFromAString.java
@@ -4,7 +4,7 @@ public class ParsingValuesFromAString {
 
         /*
             Similar to casting, parsing a value from a string is essentially getting a number in a string format
-                and parsing(converting) it into a number (int, short, byte, long, double, etc...)
+                and parsing(converting) it into a number (byte, short, int, long, float, and double)
         */
 
         String year = "2020";
@@ -16,6 +16,25 @@ public class ParsingValuesFromAString {
         // To fix this we need to parse the string
         int nextYear = Integer.parseInt(year) + 1; // This parses(converts) the year into a number and adds 1 to it as a number and not a string
         System.out.println("Next year will be: " + nextYear);
+
+        System.out.println("____________________");
+
+        /*
+            This also works with other data types such as byte, short, int, long, float, and double
+        */
+
+        String temperature = "92.5";
+        float howManyDegreesUntil100 = 100 - Float.parseFloat(temperature);
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Degrees until 100: " + howManyDegreesUntil100);
+
+        System.out.println("____________________");
+
+        String size_m = "0.0128";
+        double size_cm = Double.parseDouble(size_m) * 100;
+        double size_mm = Double.parseDouble(size_m) * 1000;
+        System.out.println("Size in cm: " + size_cm);
+        System.out.println("Size in mm: " + size_mm);
 
     }
 
